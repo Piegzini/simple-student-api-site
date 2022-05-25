8df432ca389429528945f47eb0d79cac872f65d1
Konrad Piegza <piegzini.k@gmail.com>
2022-05-25 23:26:42 +0200

    Add react form hook and simple logic

--- src/routes/Register.jsx
+++ src/routes/Register.jsx
@@ -1,9 +1,24 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
 import {
   Box, Button, Flex, FormControl, FormLabel, Heading, Input,
 } from '@chakra-ui/react';
+import axios from 'axios';
 
 function Register() {
+  const { register, handleSubmit } = useForm();
+
+  const send = async (data) => {
+    try {
+      const { statusCode, message } = await axios.post('http://localhost:4000/service/register', {
+        ...data,
+      });
+      console.log(statusCode, message);
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
   return (
     <Box display="flex" flexDirection="column" justifyContent="center">
       <Box width="100%" mt="10">
@@ -12,6 +27,7 @@ function Register() {
 
       <Box width="100%" display="flex" justifyContent="center" mt="20">
         <Box
+          as="form"
           width="20%"
           display="flex"
           flexDirection="column"
@@ -19,22 +35,24 @@ function Register() {
           padding="1"
           borderRadius="10"
           borderColor="white"
+          onSubmit={handleSubmit(send)}
 
         >
           <FormControl padding="4">
             <FormLabel htmlFor="username">Username </FormLabel>
-            <Input id="username" type="username" />
+            <Input id="username" type="username" {...register('username')} required />
           </FormControl>
           <FormControl padding="4">
             <FormLabel htmlFor="email">Email </FormLabel>
-            <Input id="email" type="email" />
+            <Input id="email" type="email" {...register('email')} required />
           </FormControl>
           <FormControl padding="4">
             <FormLabel htmlFor="password">Password </FormLabel>
-            <Input id="password" type="password" />
+
+            <Input id="password" type="password" {...register('password')} required />
           </FormControl>
           <Flex mt="4" mb="4" width="100%" display="flex" justifyContent="center">
-            <Button width="50%" colorScheme="whatsapp" size="md">
+            <Button type="submit" width="50%" colorScheme="whatsapp" size="md">
               Register
             </Button>
           </Flex>
